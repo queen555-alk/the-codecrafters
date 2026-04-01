b8ce124f25b459b4651319c51296dadd46c652e4
jadah <adahjeremiah3@gmail.com>
2026-04-01 20:44:54 +0100

    compound markers updated

---
+++ sentinel-reborn/functions/hex.go
@@ -0,0 +1,25 @@
+package functions
+
+import (
+	"log"
+	"strconv"
+	"strings"
+)
+
+func HexToBin(words string) string {
+	input := strings.Fields(words)
+
+	for i := 1; i < len(input); i++ {
+		if input[i] == "(hex)" && i > 0 {
+			val, err := strconv.ParseInt(input[i-1], 16, 64)
+			if err != nil {
+				log.Fatal(err)
+				continue
+			}
+			input[i-1] = strconv.FormatInt(val, 10)
+			input = append(input[:i], input[i+1:]...)
+			i--
+		}
+	}
+	return strings.Join(input, " ")
+}
