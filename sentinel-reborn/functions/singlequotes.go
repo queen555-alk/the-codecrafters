8e58983e76558ea37bf2bd323e8b5b88a3a0dac2
Timothy Ododo <timothyododo@gmail.com>
2026-04-01 19:03:02 +0100

    capitalize first letter of function name so its exportable.

--- sentinel-reborn/functions/singlequotes.go
+++ sentinel-reborn/functions/singlequotes.go
@@ -4,7 +4,7 @@ import (
 	"regexp"
 )
 
-func singleQuotes(s string) string {
+func SingleQuotes(s string) string {
 	re := regexp.MustCompile(`'\s*(.*?)\s*'`)
 
 	return re.ReplaceAllString(s, "'$1'")
